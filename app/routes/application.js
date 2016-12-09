ed4d8ec2237eca0756158bd99dd63419554cf77a
Thomas Niedermaier <thomas.niedermaier@gmx.at>
2016-12-09 16:15:51 +0100

    pulled from master

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,11 +1,13 @@
+// app/routes/application.js
 import Ember from 'ember';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
-import { default as math, PI}  from 'math';
-const { Route, inject } = Ember;
+import { default as math, PI } from 'math';
+
+const { Route, inject, getOwner } = Ember;
 
 export default Route.extend(ApplicationRouteMixin, {
     session: inject.service(),
-
+    currentUser: inject.service(),
     actions: {
         logout() {
             this.get('session').invalidate();
@@ -14,11 +16,15 @@ export default Route.extend(ApplicationRouteMixin, {
     beforeModel() {
         this._super(...arguments);
         console.log(math, PI);
+        if (this.get('session.isAuthenticated')) {
+            return this.get('currentUser').loadUserInfo();
+        }
     },
-
     setupController(controller) {
-      this._super(...arguments);
-      const coords = Ember.getOwner(this).lookup('data:location');
-      controller.set('coords', coords);
+        this._super(...arguments);
+        let locObj = getOwner(this).lookup('data:location');
+        let reqObj = getOwner(this).lookup('data:request');
+        controller.set('loc', locObj);
+        controller.set('req', reqObj);
     }
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,43 +2,30 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var Filter = require('broccoli-filter');
-var debugTree = require('broccoli-stew').debug;
-
-function Note(inputNode) {
-    Filter.call(this, inputNode);
-}
 
 module.exports = function(defaults) {
     var app = new EmberApp(defaults, {
         // Add options here
     });
 
-    // Use `app.import` to add additional libraries to the generated
-    // output files.
-    //
-    // If you need to use different assets in different
-    // environments, specify an object as the first parameter. That
-    // object's keys should be the environment name and the values
-    // should be the asset to use in that environment.
-    //
-    // If the library that you are including contains AMD or ES6
-    // modules that you would like to import into your application
-    // please specify an object with the list of modules as keys
-    // along with the exports of each module as its value.
+    function MyFilter(inputNode) {
+        Filter.call(this, inputNode);
+    }
 
-    Note.prototype = Object.create(Filter.prototype);
-    Note.prototype.processString = function(existingString) {
-        return `/**
+    MyFilter.prototype = Object.create(Filter.prototype);
+
+    MyFilter.prototype.processString = function(existingString) {
+        var prepend = `/**
             * vendor.js
             *
             * (c) 2016 🦄🦄🦄🔫🌈🍺🍺 All Rights Reserved
-            * generated at: ${ new Date().toISOString() }
-            */
-
-            ${existingString}`;
+            * generated at: ${(new Date()).toISOString()}
+            */`;
+        return prepend + existingString;
     };
-    Note.prototype.extensions = ['js'];
-    Note.prototype.targetExtension = 'js';
 
-    return new Note(app.toTree());
+    MyFilter.prototype.extensions = ['js'];
+    MyFilter.prototype.targetExtension = 'js';
+
+    return new MyFilter(app.toTree());
 };
